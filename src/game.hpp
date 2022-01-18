8b39051833b0c7488b1abc490391e6efb285c1f4
thetomato2 <derekj.nord@gmail.com>
2022-01-18 15:49:08 -0600

    reversed color order - I was packing it backwards

--- src/game.hpp
+++ src/game.hpp
@@ -169,10 +169,10 @@ struct Color_u32
         u32 argb;
         struct
         {
-            u8 a;
-            u8 r;
-            u8 g;
             u8 b;
+            u8 g;
+            u8 r;
+            u8 a;
         };
     };
 };
